02654ad7cb9eafcc4914de084453e7aa7e1df702
Manny G <manikandan@live.com>
2025-01-22 00:26:09 -0800

    Added test project

---
+++ SimpleObjectComparer.Tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleObjectComparer.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+        }
+    }
+}
